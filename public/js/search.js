80779fdfe175b4a9b0d6b37c4cef8f02ab665175
Bachynin Ivan <bachynin.i@gmail.com>
2017-07-15 22:04:02 +0300

    🎨💄 separate search result template

--- public/js/search.js
+++ public/js/search.js
@@ -2,12 +2,13 @@ $(document).ready(function () {
     const source = $("#search-template").html();
     const template = Handlebars.compile(source);
 
-    $('#search').click(function (e) {
+    $('#search-btn').click(function (e) {
+        console.log('search');
         e.preventDefault();
         $.ajax({
             type: "GET",
             url: "/search",
-            data: {query: $("#query").val()},
+            data: {search: $("#search").val()},
             success: function (result) {
                 const html = template(result);
                 $('#search-result').html(html);
