c2fcdb297a5f39ac49726cf8a607ce50993dc56d
Bachynin Ivan <bachynin.i@gmail.com>
2017-07-15 22:03:25 +0300

    🎨 switch view engine to pugjs

--- app.js
+++ app.js
@@ -7,34 +7,13 @@ const bodyParser = require('body-parser');
 const handlebars = require('express-handlebars');
 
 const database = require('./lib/database');
-
 const indexRouter = require('./routes/index');
-const apiSetup = require('./routes/api');
-const staticHelper = require('./lib/static');
 
 
 const app = express();
 
 
-// set up handlebars view engine
-app.engine('hbs', handlebars.create({
-    defaultLayout: 'main',
-    extname: '.hbs',
-    helpers: {
-        'static': function(name) {
-            return staticHelper.map(name)
-        },
-        'section': function(name, options){
-            if(!this._sections)
-                this._sections = {};
-            this._sections[name] = options.fn(this);
-            return null;
-        },
-    }
-}).engine);
-app.set('view engine', 'hbs');
-
-
+app.set('view engine', 'pug');
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -42,7 +21,6 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// apiSetup.setup(app);
 app.use('/', indexRouter);
 
 
