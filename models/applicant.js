151ef33cc945babc6585cbf417a6c794361a52ae
Bachynin Ivan <bachynin.i@gmail.com>
2017-07-21 23:13:14 +0300

    🍃 add name transliteration for applicants

--- models/applicant.js
+++ models/applicant.js
@@ -6,15 +6,19 @@ const mongoose = require('mongoose'),
  * @typedef {Object} Model.Applicant
  * @property {ObjectId} _id
  * @property {String} name
+ * @property {String} slug
  * @property {ObjectId[]} applications
  */
 const applicantSchema = new Schema({
     name: {type: String, required: true, max: 100, trim: true},
-    applications: [{type: ObjectId, ref: 'Application'}]
+    trName: {type: String, required: true, max: 100, trim: true},
+    slug: {type: String, required: true, max: 100, trim: true},
+    applications: [{type: ObjectId, ref: 'Application'}],
 });
 
 applicantSchema.virtual('url').get(function () {
-    return `/applicants/${this._id}`
+    // return `/applicants/${this._id}`
+    return `/applicants/${this.slug}`
 });
 
 const Applicant = mongoose.model('Applicant', applicantSchema);
